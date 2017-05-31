db9041b2e4166be135565110f8036c1f9eee22aa
Edward <evnguyen@uwaterloo.ca>
2017-05-31 17:50:55 +0000

    comp3 now shows 3 blocks

--- templates/webform-confirmation-19.tpl.php
+++ templates/webform-confirmation-19.tpl.php
@@ -21,109 +21,249 @@
 ?>
 
 <?php
-  //Global declarations
-  $sid = $_GET['sid'];
+/**
+ * Declaration of global variable.
+ * This section declares the following list of variables to have global access.
+ * Having global access lets the variable be used within a function
+ */
   global $submission;
+  global $comp1_all_courses;
   global $comp1_ahs_courses;
   global $comp1_env_courses;
   global $comp1_math_courses;
+  global $comp2_all_courses;
   global $comp2_arts_all_courses;
   global $comp2_arts_psci_courses;
-  $submission = webform_get_submission($node->nid, $sid);
-    //$submission->data[i][j] where i is the question number and j is = 0
-
-  $comp1_ahs_courses = array(
-    1 => "AHS 107",
-    2 => "CCA workshop",);
-  $comp1_env_courses = array(
-    1 => "ENVS 178",
-    2 => "CCA workshop",);
-  $comp1_math_courses = array(
-    1 => "ENGL 119",
-    2 => "CCA workshop",);
-  $comp2_arts_all_courses = array(
-    1 => "PSCI 299",
-    2 => "PD1", );
-  $comp2_arts_psci_courses = array(
-    1 => "PSCI 299",
-    2 => "PD1", );
-
-  //Debug
-  //dsm($submission);
-  //dsm($node);
-  $test = webform_submission_data($node, $submission->data);
-  //dsm($test);
 
 /**
+ * Variable definitions.
+ * This section defines the values associated with each variable.
+ * In order to access submission values, syntax is as follows:
+ * $submission->data[i][j] where i is the question number and j is 0
+ * TODO: Change variable names for compX_all_courses
+ */
+  $sid = $_GET['sid'];
+  $submission = webform_get_submission($node->nid, $sid);
+
+  $comp1_all_courses = array(
+    1 => "CCA Workshop",
+    );
+  $comp1_ahs_courses = array(
+    1 => "AHS 107",
+    2 => "CCA Workshop",
+    );
+  $comp1_env_courses = array(
+    1 => "ENVS 178",
+    2 => "CCA Workshop",
+    );
+  $comp1_math_courses = array(
+    1 => "ENGL 119",
+    2 => "CCA Workshop",
+    );
+
+  $comp2_all_courses = array(
+    1 => "PD1",
+    );
+  $comp2_arts_all_courses = array(
+    1 => "ARTS 111",
+    2 => "PD1",
+    );
+  $comp2_arts_psci_courses = array(
+    1 => "PSCI 299",
+    2 => "ARTS 111",
+    3 => "PD1",
+    );
+
+  $comp3_arts_drama_courses = array(
+    1 => "DRAMA 206",
+    2 => "DRAMA 207",
+    3 => "DRAMA 243",
+    4 => "DRAMA 244",
+    5 => "DRAMA 306",
+    6 => "DRAMA 307",
+    7 => "DRAMA 316",
+    8 => "DRAMA 317",
+    9 => "DRAMA 400",
+    10 => "DRAMA 406",
+    11 => "DRAMA 407",
+    12 => "DRAMA 416",
+    13 => "DRAMA 417",
+  );
+  $comp3_arts_fine_courses = array(
+    1 => "FINE 243",
+    2 => "FINE 343",
+  );
+  $comp3_arts_gbda_courses = array(
+    1 => "GBDA 301",
+    2 => "GBDA 302",
+    3 => "GBDA 401",
+    4 => "GBDA 402",
+  );
+  $comp3_arts_ger_courses = array(
+    1 => "GER 407",
+  );
+  $comp3_arts_ls_courses = array(
+    1 => "LS 434",
+  );
+  $comp3_arts_pac_courses = array(
+    1 => "PAC 390",
+    2 => "PAC 395",
+  );
+  $comp3_arts_psci_courses = array(
+    1 => "PSCI 498C",
+  );
+  $comp3_arts_psych_courses = array(
+    1 => "PSYCH 465",
+    2 => "PSYCH 466",
+    3 => "PSYCH 467",
+  );
+  $comp3_arts_sds_courses = array(
+    1 => "SDS 370R",
+    2 => "SDS 496R",
+  );
+
+
+
+
+
+
+
+/**
+ * @param $list -> an array which holds the list of elements to be chosen from
+ * @return string
+ * Helper function for getting a random element in an array
+ */
+  function getRandomElement($list){
+    $min = 1;
+    $max = count($list);
+    $index = rand($min, $max);
+    $retval = $list[$index];
+    return $retval;
+  }
+
+/**
+ * @return string
  * Generate the results for Component 1: Skills identification
  */
+  //TODO: Change if conditions to switch statements if required
   function getComp1(){
     global $submission;
+    global $comp1_all_courses;
+    global $comp1_ahs_courses;
+    global $comp1_env_courses;
+    global $comp1_math_courses;
+
+    if($submission->data[2][0] == "AHS"){
+      $retval = getRandomElement($comp1_ahs_courses);
+      return $retval;
+    }
+    elseif ($submission->data[2][0] == "ENV"){
+      $retval = getRandomElement($comp1_env_courses);
+      return $retval;
+    }
+    elseif ($submission->data[2][0] == "MATH"){
+      $retval = getRandomElement($comp1_math_courses);
+      return $retval;
+    }
+    else{
+      $retval = getRandomElement($comp1_all_courses);
+      return $retval;
+    }
 
   }
 
 /**
+ * @return string
  * Generate the results for Component 2: Career Development Course
  */
   function getComp2(){
     global $submission;
+    global $comp2_all_courses;
     global $comp2_arts_all_courses;
     global $comp2_arts_psci_courses;
 
     if($submission->data[2][0] == "ART" && $submission->data[4][0] == "PSCI"){
-      $max = count($comp2_arts_psci_courses);
-      $min = 1;
-      $index = rand($min, $max);
-      $retval = $comp2_arts_psci_courses[$index];
+      $retval = getRandomElement($comp2_arts_psci_courses);
+      return $retval;
+    }
+    elseif($submission->data[2][0] == "ART"){
+      $retval = getRandomElement($comp2_arts_all_courses);
       return $retval;
     }
     else{
-      $max = count($comp2_arts_psci_courses);
-      $min = 1;
-      $index = rand($min, $max);
-      $retval = $comp2_arts_all_courses[$index];
+      $retval = getRandomElement($comp2_all_courses);
       return $retval;
     }
   }
 
+
 /**
- * Create a multiple functions that get called.
- * At least one for each component.
+ * @return string
+ * Generate the results for Component 3: Work/Community Experiences
  */
+  function getComp3(){
+    global $submission;
+    global $comp2_all_courses;
+    global $comp2_arts_all_courses;
+    global $comp2_arts_psci_courses;
+
+    if($submission->data[2][0] == "ART" && $submission->data[4][0] == "PSCI"){
+      $retval = getRandomElement($comp2_arts_psci_courses);
+      return $retval;
+    }
+    elseif($submission->data[2][0] == "ART"){
+      $retval = getRandomElement($comp2_arts_all_courses);
+      return $retval;
+    }
+    else{
+      $retval = getRandomElement($comp2_all_courses);
+      return $retval;
+    }
+  }
+
 
 
 /**
  * @param $string -> the string of the course code
- * @return string -> The return value will have the form  https://ugradcalendar.uwaterloo.ca/courses/$string[0-i]/$string[i-j]
+ * @return string -> The return value will have the form
+ * https://ugradcalendar.uwaterloo.ca/courses/$string[0-i]/$string[i-j]
  * The purpose of this function is to generate the href for the course code.
  */
   function genLink($string){
-    $link = "https://ugradcalendar.uwaterloo.ca/courses/";
-    $course_alpha = "";
-    $course_num = "";
-    $len = strlen($string);
-    $string = str_replace(' ', '', $string);
-    for ($i = 0; $i < strlen($string); $i++){
-      $char = $string[$i];
-      if(!ctype_alpha($char)){
-        $course_num = substr($string, $i, $len);
-        break;
-      }
-      else{
-        $course_alpha .= $char;
-      }
+    //TODO: replace this placeholder link for CCA workshop
+    if($string == "CCA Workshop"){
+      return "https://uwaterloo.ca/career-action/appointments-workshops";
     }
-    $link .= $course_alpha . "/" . $course_num;
-    return $link;
+    else{
+      $link = "https://ugradcalendar.uwaterloo.ca/courses/";
+      $course_alpha = "";
+      $course_num = "";
+      $len = strlen($string);
+      $string = str_replace(' ', '', $string);
+      for ($i = 0; $i < strlen($string); $i++){
+        $char = $string[$i];
+        if(!ctype_alpha($char)){
+          $course_num = substr($string, $i, $len);
+          break;
+        }
+        else{
+          $course_alpha .= $char;
+        }
+      }
+      $link .= $course_alpha . "/" . $course_num;
+      return $link;
+    }
   }
 
 ?>
 
 <!--HTML section -->
 <!--TODO: Make the text translatable through php AND check accessibility levels-->
-<!--TODO: Adjust the alignment of the buttons -->
-<!--TODO: Hover effect bug -->
 <!--TODO: Stop webpage refresh from re-running the function calls -->
+<!--TODO: Make the width of the confirmation page wider -->
+<!--TODO: Find a clean way to incorporate course descriptions -->
+<!--TODO: Clean up dead code -->
+<!--TODO: Clean up using coding standards -->
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.js"></script>
 <div class="container-grid">
   <div class="grid-item webform-confirmation">
@@ -131,24 +271,27 @@
     experiences you can take to fulfill the components of the EDGE program</p>
   </div>
 
+  <!-- COMPONENT 1 -->
   <div class="component1 margin_top">
     <p>Component 1: Skills identification and Articulation Workshop</p>
   </div>
 
-  <?php print
+  <?php
+  $comp1 = getComp1();
+  print
   '<div class="component1_block">' .
     '<div class="width_adjust">'.
       '<div class="call-to-action-top-wrapper">'.
-        '<a href="' . genLink("AHS 107") . '"' . '>' .
+        '<a href="' . genLink($comp1) . '"' . '>' .
           '<div class="call-to-action-wrapper">' .
-            '<div class="call-to-action-theme-uWaterloo border_radius">'.
-              '<div class="call-to-action-big-text">'. $comp1_ahs_courses[1] .
+            '<div class="call-to-action-theme-uWaterloo">'.
+              '<div class="call-to-action-big-text">'. $comp1 .
               '</div>' .
             '</div>'.
           '</div>' .
         '</a>' .
       '</div>' .
-  '</div>'.
+    '</div>'.
   '</div>'
   ?>
 
@@ -158,27 +301,27 @@
     </div>
   </div>
 
+  <!-- COMPONENT 2 -->
   <div class="component2 margin_top">
     <p>Component 2: Career Development Course</p>
   </div>
 
-
   <?php
   $comp2 = getComp2();
-    print
-      '<div class="component2_block">' .
-        '<div class="width_adjust">'.
-          '<div class="call-to-action-top-wrapper">'.
-            '<a href="' . genLink($comp2) . '"' . '>' .
-              '<div class="call-to-action-wrapper">' .
-                '<div class="call-to-action-theme-uWaterloo">'.
-                  '<div class="call-to-action-big-text">'. $comp2 . '</div>' .
-                '</div>'.
-              '</div>' .
-            '</a>' .
-          '</div>' .
-        '</div>'.
-      '</div>'
+  print
+    '<div class="component2_block">' .
+      '<div class="width_adjust">'.
+        '<div class="call-to-action-top-wrapper">'.
+          '<a href="' . genLink($comp2) . '"' . '>' .
+            '<div class="call-to-action-wrapper">' .
+              '<div class="call-to-action-theme-uWaterloo">'.
+                '<div class="call-to-action-big-text">'. $comp2 . '</div>' .
+              '</div>'.
+            '</div>' .
+          '</a>' .
+        '</div>' .
+      '</div>'.
+    '</div>'
   ?>
 
 
@@ -188,10 +331,86 @@
     </div>
   </div>
 
-  <div class="component3">
+  <!-- COMPONENT 3 -->
+  <div class="component3 margin_top">
     <p>Component 3: Work/Community Experiences</p>
   </div>
 
+  <?php
+  $comp2 = "Comp3";
+  print
+    '<div class="component3_block1">' .
+      '<div class="width_adjust">'.
+        '<div class="call-to-action-top-wrapper">'.
+          '<a href="' . genLink($comp2) . '"' . '>' .
+            '<div class="call-to-action-wrapper">' .
+              '<div class="call-to-action-theme-uWaterloo">'.
+                '<div class="call-to-action-big-text">'. $comp2 . '</div>' .
+              '</div>'.
+            '</div>' .
+          '</a>' .
+        '</div>' .
+      '</div>'.
+    '</div>'
+  ?>
+
+  <div class="component3_descr1">
+    <div>
+      <p>Some text that describes the block. Some text that describes the block. Some text that describes the block. Some text that describes the block. Some text that describes the block.</p>
+    </div>
+  </div>
+
+  <?php
+  $comp2 = "Comp3";
+  print
+    '<div class="component3_block2">' .
+      '<div class="width_adjust">'.
+        '<div class="call-to-action-top-wrapper">'.
+          '<a href="' . genLink($comp2) . '"' . '>' .
+            '<div class="call-to-action-wrapper">' .
+              '<div class="call-to-action-theme-uWaterloo">'.
+                '<div class="call-to-action-big-text">'. $comp2 . '</div>' .
+              '</div>'.
+            '</div>' .
+          '</a>' .
+        '</div>' .
+      '</div>'.
+    '</div>'
+  ?>
+
+  <div class="component3_descr2">
+    <div>
+      <p>Some text that describes the block. Some text that describes the block. Some text that describes the block. Some text that describes the block. Some text that describes the block.</p>
+    </div>
+  </div>
+
+  <?php
+  $comp2 = "Comp3";
+  print
+    '<div class="component3_block3">' .
+      '<div class="width_adjust">'.
+        '<div class="call-to-action-top-wrapper">'.
+          '<a href="' . genLink($comp2) . '"' . '>' .
+            '<div class="call-to-action-wrapper">' .
+              '<div class="call-to-action-theme-uWaterloo">'.
+                '<div class="call-to-action-big-text">'. $comp2 . '</div>' .
+              '</div>'.
+            '</div>' .
+          '</a>' .
+        '</div>' .
+      '</div>'.
+    '</div>'
+  ?>
+
+  <div class="component3_descr3">
+    <div>
+      <p>Some text that describes the block. Some text that describes the block. Some text that describes the block. Some text that describes the block. Some text that describes the block.</p>
+    </div>
+  </div>
+
+
+
+  <!-- COMPONENT 4 -->
   <div class="component4">
     <p>Component 4: Capstone Workshop</p>
   </div>
